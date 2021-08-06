824f6ba35e82f23384756ed5c6a1819184d9966b
AndreSmrnv <tehmir@bk.ru>
2021-08-06 22:13:50 +0300

    add init stor

--- 06-express_2/index.js
+++ 06-express_2/index.js
@@ -1,30 +1,9 @@
 #!/usr/bin/env node
 const express = require('express');
 const cors = require('cors');
-
-const appConfig = require('./config');
-
 const {Book, User} = require('./models');
-const stor = {
-    books: [],
-    users: []
-};
-
-stor.users.push(new User("test@mail.ru", 1));
-
-[1, 2, 3, 4, 5].map(el => {
-    const newBook = new Book(
-        `book ${el}`,           //title
-        `desc book ${el}`,      //description
-        `authors book ${el}`,   //authors
-        `favorite book ${el}`,  //favorite
-        `fileCover book ${el}`, //fileCover
-        `fileName book ${el}`   //fileName
-    );
-    stor.books.push(newBook);
-});
-
-
+const appConfig = require('./config');
+const stor = require('./store');
 
 const app = express();
 app.use(cors());
